e245c9ffed2c7d64873ebaffa483206992b21e93
abecombe <abecombe@gmail.com>
2025-04-06 18:21:13 +0900

    small change

---
+++ Packages/com.abecombe.onesweep/Runtime/ComputeShaders/OnesweepCommon/PartitionDescriptor.hlsl
@@ -0,0 +1,25 @@
+﻿#ifndef CS_ONE_SWEEP_COMMON_PARTITION_DESCRIPTOR_HLSL
+#define CS_ONE_SWEEP_COMMON_PARTITION_DESCRIPTOR_HLSL
+
+#define FLAG uint
+#define PARTITION_DESCRIPTOR uint
+static const FLAG FLAG_INVALID   = 0u;
+static const FLAG FLAG_AGGREGATE = 1u;
+static const FLAG FLAG_PREFIX    = 2u;
+static const uint partition_descriptor_flag_mask = 0x00000003u;
+static const uint partition_descriptor_value_shift = 2u;
+
+inline PARTITION_DESCRIPTOR create_partition_descriptor(in uint value, in FLAG flag)
+{
+    return (value << partition_descriptor_value_shift) | flag;
+}
+inline FLAG get_partition_descriptor_flag(in PARTITION_DESCRIPTOR partition_descriptor)
+{
+    return partition_descriptor & partition_descriptor_flag_mask;
+}
+inline uint get_partition_descriptor_value(in PARTITION_DESCRIPTOR partition_descriptor)
+{
+    return partition_descriptor >> partition_descriptor_value_shift;
+}
+
+#endif /* CS_ONE_SWEEP_COMMON_PARTITION_DESCRIPTOR_HLSL */
--- Packages/com.abecombe.onesweep/Runtime/ComputeShaders/OnesweepRadixSort/PartitionDescriptor.hlsl
+++ Packages/com.abecombe.onesweep/Runtime/ComputeShaders/OnesweepRadixSort/PartitionDescriptor.hlsl
@@ -1,28 +1,9 @@
 ﻿#ifndef CS_ONE_SWEEP_RADIX_SORT_PARTITION_DESCRIPTOR_HLSL
 #define CS_ONE_SWEEP_RADIX_SORT_PARTITION_DESCRIPTOR_HLSL
 
+#include "../OnesweepCommon/PartitionDescriptor.hlsl"
 #include "Radix.hlsl"
 
-#define FLAG uint
-#define PARTITION_DESCRIPTOR uint
-static const FLAG FLAG_INVALID   = 0u;
-static const FLAG FLAG_AGGREGATE = 1u;
-static const FLAG FLAG_PREFIX    = 2u;
-static const uint partition_descriptor_flag_mask = 0x00000003u;
-static const uint partition_descriptor_value_shift = 2u;
-
-inline PARTITION_DESCRIPTOR create_partition_descriptor(in uint value, in FLAG flag)
-{
-    return (value << partition_descriptor_value_shift) | flag;
-}
-inline FLAG get_partition_descriptor_flag(in PARTITION_DESCRIPTOR partition_descriptor)
-{
-    return partition_descriptor & partition_descriptor_flag_mask;
-}
-inline uint get_partition_descriptor_value(in PARTITION_DESCRIPTOR partition_descriptor)
-{
-    return partition_descriptor >> partition_descriptor_value_shift;
-}
 inline uint get_partition_descriptor_buffer_address(in uint bucket_id, in uint partition_index, in uint group_size, in uint radix_step)
 {
     return RADIX_BASE * (group_size * radix_step + partition_index) + bucket_id;
